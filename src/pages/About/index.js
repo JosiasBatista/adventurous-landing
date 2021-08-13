76a2ac126ce55d4ccacd34a08a789c93f723b653
Josias Leal <josias.leal@accenture.com>
2021-08-13 20:39:07 -0300

    Finalizando estruturação da página About

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header'
 
 import './style.css';
+import ChevronRight from '../../public/chevron-right.svg';
+import ChevronLeft from '../../public/chevron-left.svg';
 
 export default function AboutPage() {
+    const [actualNumberMessage, setActualNumberMessag] = useState(0)
+    const [messages, ] = useState([
+        "Find people with the same \n interestes",
+        "Join communities of what \n you love"            
+    ])
+
+    function getActualMessage() {
+        return messages[actualNumberMessage]
+    }
+
     return (
         <div className="about-page_container">
             <Header />
 
-            <div className="about-page_introduction"></div>
+            <div className="about-page_introduction">
+            </div>
+
             <section className="about-page_introduction-title">
                 <h1>ADVENTURES</h1>
                 <h1>NEEDS</h1>
@@ -21,13 +35,38 @@ export default function AboutPage() {
                 <p className="app-motivation-desc text">Have you ever been in a situation{"\n"}where you were looking for a partner{"\n"}or a group of people to play a sport?</p>
             </section>
 
-            <section className="apps-possibilities">
+            <section className="apps-possibilities" scroll="no">
                 <aside className="gym-buddies-photo">
+                    <span className="text">Maybe a gym partner</span>
                 </aside>
 
                 <aside className="skate-buddies-photo">
+                    <span className="text">Or people to teach you how{"\n"}to skateboard...</span>
                 </aside>
             </section>
+
+            <section className="app-existence-desc">
+                <p className="subtitle">It doesn’t matter... Life is better when{"\n"}
+                SHARED and we exist to help you{"\n"}
+                SHARE and <span className="strong-green-text">LIVE</span> in community
+                </p>
+
+                <div className="app-existence-desc-box" >
+                    <p className="text">{getActualMessage()}</p>
+
+                    {actualNumberMessage < messages.length - 1 ?
+                        <button onClick={() => setActualNumberMessag(actualNumberMessage + 1)} className="invisible-button right">
+                            <img src={ChevronRight} alt="right"/>
+                        </button>
+                        :
+                        <button onClick={() => setActualNumberMessag(actualNumberMessage - 1)} className="invisible-button left">
+                            <img src={ChevronLeft} alt="left"/>
+                        </button>
+                    }
+                </div>
+            </section>
+
+            <button className="download-button">Join US</button>
         </div>
     )
 }
